b240f354a91ea86d4f79f4803dab49491a88162d
JoanaP02 <a97588@alunos.uminho.pt>
2022-04-12 16:41:01 +0100

    Inicializacao de mais algumas classes

--- src/main/java/SmartCamera.java
+++ src/main/java/SmartCamera.java
@@ -1,3 +1,57 @@
+
 public class SmartCamera extends SmartDevice {
-    //METODO CLONE
+    private double resolution;
+    private double size;
+
+    public SmartCamera () {
+        super();
+        this.resolution=0.0;
+        this.size = 0.0;
+    }
+
+    public SmartCamera (String id, double re, double size){
+        super(id);
+        this.resolution = re;
+        this.size = size;
+    }
+
+    public SmartCamera (SmartCamera sc){
+        this.resolution = sc.getResolution();
+        this.size = sc.getSize();
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public double getResolution(){
+        return this.resolution;
+    }
+
+    public void setResolution(double resolution) {
+        this.resolution = resolution;
+    }
+
+    public String toString() {
+        return "SmartCamera{" +
+                "resolution=" + resolution +
+                ", size=" + size +
+                '}';
+    }
+
+    public double consumoEnergia(){
+        return this.resolution * this.size;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SmartCamera)) return false;
+        SmartCamera that = (SmartCamera) o;
+        return Double.compare(that.getResolution(), getResolution()) == 0 && Double.compare(that.getSize(), getSize()) == 0;
+    }
+
 }
